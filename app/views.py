8ece8399052039bd3330c4ac5a28b373dbc29337
Rhys Hansen <rhys.hansen@videopropulsion.com>
2017-01-14 20:12:18 -0600

    Switch Bootstrap and JQuery to NPM management. General progress

--- app/views.py
+++ app/views.py
@@ -1,26 +1,29 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, jsonify, redirect
+from app.extensions.image_color_average import imgavg as ia
+import pprint
+
+def rgb_to_hex(red, green, blue):
+    """Return color as #rrggbb for the given color values."""
+    return '#%02x%02x%02x' % (red, green, blue)
+
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index/')
 def index():
     return render_template("home.html")
 
-@app.route('/upload/')
+@app.route('/single_average/')
 def upload():
     """Test File Upload."""
-    return render_template('users/upload.html')
+    return render_template('single_average.html')
 
-def transform(text_file_contents):
-    return text_file_contents.replace("=", ",")
-
-@app.route('/upload/bounce', methods=["POST"])
-def transform_view():
-    file = request.files['data_file']
-    if not file:
-        return "No file"
-    file_contents = file.stream.read().decode("utf-8")
-    result = transform(file_contents)
-    response = make_response(result)
-    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
-    return response
+@app.route('/single_average/_average_single', methods=['POST','GET'])
+def upload_test():
+    result = '#'
+    color = ['upload', 255, 255, 255]
+    f = request.files['file']
+    color = ia.avg_single_img(f, name='upload')
+    result = rgb_to_hex(color[1],color[2],color[3])
+    return jsonify(result=result, color=color)
