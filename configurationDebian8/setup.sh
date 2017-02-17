6a6d81464a30c361fbe8f2c18ffc56e6ca105a7f
Rhys Hansen <rhyshonline@gmail.com>
2017-02-17 10:40:56 -0600

    Added files and script for Debian 8 configuration

---
+++ configurationDebian8/setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+apt-get install virtualenv python3-dev nginx-full
+pip3 install uwsgi
+
+cp rhys.miad.edu.conf /etc/nginx/sites-enabled/rhys.miad.edu.conf
+systemctl enable nginx
+systemctl reload nginx
+systemctl status nginx.service
+
+mkdir /etc/uwsgi/vassals
+cp emperor.ini /etc/uwsgi/emperor.ini
+cp uwsgi.service /lib/systemd/system/uwsgi.service
+cp rhysprojectsite.ini /etc/uwsgi/vassals/rhysprojectsite.ini
+systemctl enable uwsgi.service
+systemctl start uwsgi
