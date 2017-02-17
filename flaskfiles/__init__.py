fd8182b14605b41b5769d31d9f6e6132d6832626
Rhys Hansen <rhyshonline@gmail.com>
2017-02-16 21:56:43 -0600

    Log configuration fixed

--- flaskfiles/__init__.py
+++ flaskfiles/__init__.py
@@ -1,17 +1,29 @@
+import logging
+from logging.handlers import RotatingFileHandler
 
 from flask import Flask
+
 app = Flask(__name__)
 
 from flaskfiles import views
 
-if app.debug is not True:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    logger = logging.getLogger(__name__)
-    file_handler = RotatingFileHandler('flask.log', maxBytes=1024 * 1024 * 100, backupCount=20)
-    file_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
-    app.logger.addHandler(file_handler)
 
-app.logger.info('app.logger')
+
+file_handler = RotatingFileHandler('logs/flask.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+app.logger.info('Flask logger configured')
+
+colorlogger = logging.getLogger('imagecolor')
+colorlogger.addHandler(file_handler)
+colorlogger.setLevel(logging.WARNING)
+colorlogger.info('imagecolor logger configured')
+colorlogger.info('imagecolor v%s',views.imagecolor.__version__)
+
+searchlogger = logging.getLogger('searchcolor')
+searchlogger.addHandler(file_handler)
+searchlogger.setLevel(logging.WARNING)
+searchlogger.info('searchcolor logger configured')
+searchlogger.info('searchcolor v%s',views.searchcolor.__version__)
