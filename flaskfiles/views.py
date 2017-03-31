cbe83457f4c39de9108c27d83a4a6a84ec6c15fd
Rhys Hansen <rhyshonline@gmail.com>
2017-03-31 10:28:37 -0500

    Reformatting site for thesis display

--- flaskfiles/views.py
+++ flaskfiles/views.py
@@ -39,34 +39,11 @@ def rgb_to_hex(red, green, blue):
     return '#%02x%02x%02x' % (red, green, blue)
 
 @app.route('/')
-@app.route('/index/')
-def index():
-    return render_template("home.html")
-
-@app.route('/single_average/')
-def single_average():
-    """Test File Upload."""
-    return render_template('single_average.html')
-
-@app.route('/single_average/_average_single', methods=['POST'])
-def average_upload_image():
-    start = time()
-    result = '#'
-    color = ['upload', 255, 255, 255]
-    f = request.files['file']
-    color = imagecolor.average(f, name='upload')
-    red = color.get('red')
-    green = color.get('green')
-    blue = color.get('blue')
-    result = rgb_to_hex(red, green, blue)
-    processing_time = time() - start
-    app.logger.info('Image Average response took %0.3f seconds - R:%d, G:%d, B:%d, HEX:%s',processing_time, red, green, blue, result)
-    return jsonify(result=result, red=red, green=green, blue=blue)
-
 @app.route('/search_average/')
 def search_average():
     """Test File Upload."""
     return render_template('search_average.html')
+
 
 @app.route('/search_average/_search_single', methods=['POST'])
 def average_search_images():
